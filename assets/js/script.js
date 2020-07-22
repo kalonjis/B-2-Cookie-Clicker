28e872879c2d149781fed5a0f52e30e052ed3e62
Robin Jacquemart <jacquemart.rob@gmail.com>
2020-07-22 17:01:06 +0200

    Ajouts du Sass début

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 (() => {
   // Variable global au projet
-  
+
   const btnClick = document.getElementById('click');
   const counter = document.getElementById('counter');
   const mult = document.getElementById('mult');
@@ -13,36 +13,45 @@
   const bonusTimer = document.getElementById('bonus-timer');
   const bonus = document.getElementById('bonus');
 
-  
-  const varInit = { multPrice : 20, autoPrice : 100, bonusPrice: 1}; 
+
+  const varInit = { multPrice: 20, autoPrice: 100, bonusPrice: 250 };
   let bonusTime = null;
 
   const reset = document.getElementById('reset');
 
   if (window.localStorage.getItem('counter') === null) {
     init();
-    
-  }else {
+  } else {
     refresh();
   }
 
   btnClick.addEventListener('click', () => {
-    const mult = parseInt(window.localStorage.getItem('mult') )+1;
-   addCounter(mult);
+    const mult = parseInt(window.localStorage.getItem('mult')) + 1;
+    addCounter(mult);
   });
   reset.addEventListener('click', () => {
     init();
   });
+
+  function init() {
+    window.localStorage.setItem('counter', 0);
+    window.localStorage.setItem('mult', 0);
+    window.localStorage.setItem('auto-rate', 0);
+    window.localStorage.setItem('bonus-rate', 0);
+    refresh();
+  }
   function refresh() {
     counter.innerText = window.localStorage.getItem('counter');
     //section mult
-    multPrice.innerText = "Price : "+ fmultPrice();
-    multBy.innerText = "Mult By : "+(parseInt(window.localStorage.getItem('mult')) +1);
+    multPrice.innerText = "Price : " + fmultPrice();
+    multBy.innerText = "Mult By : " + (parseInt(window.localStorage.getItem('mult')) + 1);
     //section auto
-    autoPrice.innerText = "Price : "+ fautoPrice();
-    autoRate.innerText = "Per sec : "+ window.localStorage.getItem('auto-rate');
+    autoPrice.innerText = "Price : " + fautoPrice();
+    autoRate.innerText = "Per sec : " + window.localStorage.getItem('auto-rate');
+    //section bonus
     bonusPrice.innerText = "Price : " + fbonusPrice();
     bonusTimer.innerText = "Timer : " + fbonusTimer();
+    //Affichage des boutons si disabled
     buttonCheck();
   }
   function buttonCheck() {
@@ -51,92 +60,79 @@
     autoClick.disabled = count < fautoPrice();
     bonus.disabled = count < fbonusPrice() || bonusTime != null;
   }
-  function init() {
-    window.localStorage.setItem('counter', 0);
-    window.localStorage.setItem('mult', 0);
-    window.localStorage.setItem('auto-rate', 0);
-    window.localStorage.setItem('bonus-rate', 0);
-    refresh();
-  }
 
   mult.addEventListener('click', () => {
-    let count = parseInt(window.localStorage.getItem('counter'));
-    if  (count >= fmultPrice()){
-      let mult = parseInt(window.localStorage.getItem('mult'));
-      count-= fmultPrice();
-      window.localStorage.setItem('counter',count);
-      mult += 1;
-      window.localStorage.setItem('mult', mult);
-      
-    }
-    refresh();
+    buy(fmultPrice, 'mult');
   });
   autoClick.addEventListener('click', () => {
-    
-    let count = parseInt(window.localStorage.getItem('counter'));
-    if  (count >= fautoPrice()){
-      let auto = parseInt(window.localStorage.getItem('auto-rate'));
-      count-= fautoPrice();
-      window.localStorage.setItem('counter',count);
-      auto += 1;
-      window.localStorage.setItem('auto-rate', auto);    
-    }
-    refresh();
+    buy(fautoPrice, 'auto-rate');
   });
   bonus.addEventListener('click', () => {
-    let count = parseInt(window.localStorage.getItem('counter'));
-    if  (count >= fbonusPrice()){
-      let bonus = parseInt(window.localStorage.getItem('bonus-rate'));
-      count-= fbonusPrice();
-      window.localStorage.setItem('counter',count);
-      bonus += 1;
-      window.localStorage.setItem('bonus-rate', bonus);
-      bonusTime = Date.now() ;
+    if (buy(fbonusPrice, 'bonus-rate')) {
+      bonusTime = Date.now();
     }
-    refresh();
   });
-  function fautoPrice () {
-    let auto = parseInt(window.localStorage.getItem('auto-rate'));
-    return Math.ceil(varInit.autoPrice * Math.pow(1.15, auto));
+
+  function buy(fPrice, rate) {
+    let count = parseInt(window.localStorage.getItem('counter'));
+    if (count >= fPrice()) {
+      let value = parseInt(window.localStorage.getItem(rate));
+      count -= fPrice();
+      window.localStorage.setItem('counter', count);
+      value += 1;
+      window.localStorage.setItem(rate, value);
+      refresh();
+      return true;
+    }
+    return false;
+  };
+
+  // Affichage du prix d'amélioration
+  function fPrice(id, coef, priceInit) {
+    let rate = parseInt(window.localStorage.getItem(id));
+    return Math.ceil(priceInit * Math.pow(coef, rate));
+  }
+  function fautoPrice() {
+    return fPrice('auto-rate', 1.15, varInit.autoPrice);
   }
 
-  function fmultPrice () {
-    let mult = parseInt(window.localStorage.getItem('mult'));
-    return Math.ceil(varInit.multPrice * Math.pow(1.15, mult));
+  function fmultPrice() {
+    return fPrice('mult', 1.15, varInit.multPrice);
   }
-  function fbonusPrice () {
-    let bonus = parseInt(window.localStorage.getItem('bonus-rate'));
-    return Math.ceil(varInit.bonusPrice * Math.pow(1, bonus));
+  function fbonusPrice() {
+    return fPrice('bonus-rate', 2, varInit.bonusPrice);
   }
+
+
   function timer() {
     let auto = parseInt(window.localStorage.getItem('auto-rate'));
     addCounter(auto);
   }
-    setInterval(timer, 1000);
+  setInterval(timer, 1000);
 
-    function addCounter(value) {
-      let count = parseInt(window.localStorage.getItem('counter'));
-      count += value * fbonus() ;
-      window.localStorage.setItem('counter',count);
-      refresh();
-    }
-    function fbonus() {
-      return bonusTime != null? 2: 1;
-      
-    }
-    function fbonusTimer(){
-      if (bonusTime != null) {
-        let seconds = Math.floor((10000 -  (Date.now() - bonusTime))/1000);
-        if (seconds <= 0){
-          bonusTime = null;
-          seconds = 0;
-        }
-        return seconds + " sec";
+  function addCounter(value) {
+    let count = parseInt(window.localStorage.getItem('counter'));
+    count += value * fbonus();
+    window.localStorage.setItem('counter', count);
+    refresh();
+  }
 
+  function fbonus() {
+    return bonusTime != null ? 2 : 1;
+  }
+  function fbonusTimer() {
+    if (bonusTime != null) {
+      let seconds = Math.floor((30000 - (Date.now() - bonusTime)) / 1000);
+      if (seconds <= 0) {
+        bonusTime = null;
+        seconds = 0;
       }
-      return "30 sec";
-    }
+      return seconds + " sec";
 
- 
+    }
+    return "30 sec";
+  }
+
+
 
 })();
